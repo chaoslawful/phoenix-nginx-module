650e547fef28508f69f78b4feb84432465340a28
Wang Xiaozhe <chaoslawful@gmail.com>
2011-10-13 16:55:49 +0800

    reformat code

--- misc/dbg_jit/jit_dbg.c
+++ misc/dbg_jit/jit_dbg.c
@@ -20,68 +20,70 @@
 
 void sighandler(int signo)
 {
-	char *val;
-	char gdb_path[255];
-	char set_logging_file[255];
-	char buf[255];
-	int status;
-	pid_t pid;
+    char *val;
+    char gdb_path[255];
+    char set_logging_file[255];
+    char buf[255];
+    int status;
+    pid_t pid;
 
-	snprintf(gdb_path, sizeof(gdb_path),
-			"%s",
-			(val = getenv(ENV_GDB_PATH)) ? val : DEFAULT_GDB_PATH);
-	snprintf(set_logging_file, sizeof(set_logging_file),
-			"set logging file %s",
-			(val = getenv(ENV_OUT_FILE)) ? val : DEFAULT_OUT_FILE);
-	snprintf(buf, sizeof(buf), "%d", getpid());
+    snprintf(gdb_path, sizeof(gdb_path),
+            "%s",
+            (val = getenv(ENV_GDB_PATH)) ? val : DEFAULT_GDB_PATH);
+    snprintf(set_logging_file, sizeof(set_logging_file),
+            "set logging file %s",
+            (val = getenv(ENV_OUT_FILE)) ? val : DEFAULT_OUT_FILE);
+    snprintf(buf, sizeof(buf), "%d", getpid());
 
-	// spawn a individual gdb process and dumping backtrace of this process
-	{
-		char* argv[] = {
-			gdb_path,
-			"--batch",
-                        "-ex", "set width 0",
-                        "-ex", "set pagination off",
-			"-ex", set_logging_file,
-			"-ex", "set logging redirect on",
-			"-ex", "set logging on",
-			"-ex", "info proc",
-			"-ex", "bt f",
-			"--pid", buf
-		};
-		char* envp[] = {
-			NULL
-		};
+    // spawn a individual gdb process and dumping backtrace of this process
+    {
+        char* argv[] = {
+            gdb_path,
+            "--batch",
+            "-ex", "set width 0",
+            "-ex", "set pagination off",
+            "-ex", set_logging_file,
+            "-ex", "set logging redirect on",
+            "-ex", "set logging on",
+            "-ex", "info proc",
+            "-ex", "bt f",
+            "--pid", buf
+        };
+        char* envp[] = {
+            NULL
+        };
 
-		if (posix_spawn(&pid, gdb_path, NULL, NULL, argv, envp) < 0) {
-			perror("Error: could not spawn gdb");
-			goto out;
-		}
-	}
+        if (posix_spawn(&pid, gdb_path, NULL, NULL, argv, envp) < 0) {
+            perror("Error: could not spawn gdb");
+            goto out;
+        }
+    }
 
-	if (waitpid(pid, &status, 0) < 0) {
-		perror("waitpid on gdb");
-		goto out;
-	}
+    if (waitpid(pid, &status, 0) < 0) {
+        perror("waitpid on gdb");
+        goto out;
+    }
 
 out:
-	// raise the signal again to crash process
-	raise(signo);
+    // raise the signal again to crash process
+    raise(signo);
 }
 
 __attribute__ ((constructor))
 void ctor()
 {
-	struct sigaction sa;
-	sa.sa_handler = sighandler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESETHAND;
-	if (sigaction(SIGILL, &sa, NULL) == -1 ||
-		sigaction(SIGFPE, &sa, NULL) == -1 ||
-		sigaction(SIGABRT, &sa, NULL) == -1 ||
-		sigaction(SIGBUS, &sa, NULL) == -1 ||
-		sigaction(SIGSEGV, &sa, NULL) == -1) {
-		perror("Could not set signal handler");
-	}
+    struct sigaction sa;
+    sa.sa_handler = sighandler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESETHAND;
+    if (sigaction(SIGILL, &sa, NULL) == -1 ||
+        sigaction(SIGFPE, &sa, NULL) == -1 ||
+        sigaction(SIGABRT, &sa, NULL) == -1 ||
+        sigaction(SIGBUS, &sa, NULL) == -1 ||
+        sigaction(SIGSEGV, &sa, NULL) == -1) {
+        perror("Could not set signal handler");
+    }
 }
+
+// vi:ts=4 sw=4 et fdm=marker
 
